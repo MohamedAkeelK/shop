31dd47a30673bf0ac1c880c0407792cf2f07e5c4
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2023-01-10 16:22:03 -0500

    working on navbar

--- client/README.md
+++ client/README.md
@@ -3,3 +3,8 @@ setup folder struc, client and api
 create a react app in client
 
 setup routing for pages with react router dom
+
+using outlet to form a layout
+
+create navbar 
+use material ui for icons
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,61 @@
 import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        <div className="left"></div>
-        <div className="center"></div>
-        <div className="right"></div>
+        <div className="left">
+          <div className="item">
+            <img src="/img/en.png" alt="" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Children</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link to="/">ECOMM</Link>
+        </div>
+        <div className="right">
+          <div className="">
+            <Link to="/">Hompage</Link>
+          </div>
+          <div className="">
+            <Link to="/">About</Link>
+          </div>
+          <div className="">
+            <Link to="/">Contact</Link>
+          </div>
+          <div className="">
+            <Link to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonIcon />
+            <FavoriteBorderIcon />
+            <div className="cartIcon">
+              <ShoppingCartCheckoutIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
